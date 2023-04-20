f6f83b158ff4f466e61d3eeb4b3ce81a05247dd1
Agustin Gimenez <70532260+AgustinGimenezFIE@users.noreply.github.com>
2023-04-20 12:55:54 -0300

    Primer Commit

--- main.c
+++ main.c
@@ -9,9 +9,28 @@ a. Según el código ASCII => C+32=c. Donde c pertenece al conjunto de caractere
 b. La finalización de una cadena de caracteres se representa mediante ́/0 ́
  */
 
+#define MAX 50
 
+void convertir(char cadena[]);
+
+void convertir(char cadena[MAX])
+{
+    int i=0;
+    for(i=0; *(cadena+i)!='\0' ; i++) // Recorre todo el string
+    {
+        if(*(cadena+i)>='a'&&*(cadena+i)<='z') // Si el caracter es mayor que a y menor que z entonces
+        {
+            *(cadena+i)=*(cadena+i)-32;  // Le resto 32 (lo paso a mayuscula en ascii)
+        }
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
+    char cadena[MAX];
+    printf("Ingrese una cadena de caracteres en minuscula y se transformara en mayuscula : \n");
+    gets(cadena);
+    printf("Texto Original : %s",cadena);
+    convertir(cadena);
+    printf("\nTexto Modificado : %s",cadena);
     return 0;
 }
